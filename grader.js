6c94435de0c19431448aa394e6a09da8a2d54b23
Sebastian Abondano <sabondano1@gmail.com>
2013-07-11 01:05:48 +0000

    modified to accept a url or file from command line

--- grader.js
+++ grader.js
@@ -64,10 +64,11 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
+        .option('-u, --url <url>', 'Path to url')
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var checkJson = checkHtmlFile(program.file || program.url, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
