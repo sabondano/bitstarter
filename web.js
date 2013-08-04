52818fbda79b682007fc754c8e69058a0aba05e9
Sebastian Abondano <sabondano1@gmail.com>
2013-08-04 03:53:06 +0000

    Changed web.js port to 5000

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
   response.send(fs.readFileSync('index.html').toString());
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
